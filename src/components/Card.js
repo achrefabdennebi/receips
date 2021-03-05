be6bb711b4f37e046e00904e7d5bd7c36d63bc3c
achref.abdennebi <achref.abdennebi.ext@suez.com>
2021-03-05 20:21:56 +0100

    feat: add dynamic image

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,11 +5,19 @@ const Card = ({ details }) => {
                                            .map((item) => <li key={item}>{item}</li> )
 
     const instructions = details.instructions.split('\n')
-                                           .map((item) => <li key={item}>{item}</li> )
+                                             .map((item) => <li key={item}>{item}</li> )
+    
+    const requireImage = chemin => {
+        try {
+            return require(`../img/${chemin}`);
+        } catch (err) {
+            return require(`../img/default.jpeg`);
+        }
+    }
     return (
         <div className="card">
             <div className="image">
-                <img src={require(`../img/${details.image}`)} alt={details.nom} />
+                <img src={requireImage(details.image)} alt={details.nom} />
             </div>
             <div className="recette">
                 <h2>{ details.nom }</h2>
