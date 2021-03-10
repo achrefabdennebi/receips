1c78f990c6779792141e0b6161eb59d2d6b21aa8
achrefabdennebi <--global>
2021-03-10 17:33:50 +0100

    add event handler

--- src/components/AjouterRecette.js
+++ src/components/AjouterRecette.js
@@ -13,16 +13,29 @@ class AjouterRecette extends Component {
         this.setState({ [name]: value})
     }
 
+    handleSubmit = event => {
+        event.preventDefault();
+        const recette = { ...this.state }
+        this.props.ajouterRecette(recette)
+        Object.keys(recette).forEach((key) => {
+            recette[key] = ''
+        })
+
+        this.setState({ ...recette })
+    }
+
+
     render () {
         return (
             <div className="card">
-                <form className="admin-form ajouter-recette" >
+                <form className="admin-form ajouter-recette" onSubmit={this.handleSubmit}>
                    <input value={this.state.nom} onChange={ this.handleChange } name="nom" type="text" placeholder="Nom de la recette" />
                    <input value={this.state.image} onChange={ this.handleChange } name="image" type="text" placeholder="Nom de l'image" />
                    <textarea value={this.state.ingredients} onChange={ this.handleChange } name="ingredients"  rows="3" placeholder="Liste des ingredients">
                    </textarea> 
                    <textarea value={this.state.instructions} onChange={ this.handleChange } name="instructions" rows="15" placeholder="Liste des instructions">
                    </textarea>
+                   <button type='submit'>+ Ajouter une recette</button>
                 </form>      
             </div>
         )
