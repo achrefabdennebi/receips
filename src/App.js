f7d4d0af35f98d981431e7ce21476443dfc1a234
achref.abdennebi <achref.abdennebi.ext@suez.com>
2021-03-10 12:38:10 +0100

    feat: add firebase connexion

--- src/App.js
+++ src/App.js
@@ -8,12 +8,22 @@ import Header from './components/Header'
 import Admin from './components/Admin'
 import Card from './components/Card'
 
+
+// Firebase
+import base from './base'
 class App extends Component {
   state = {
     pseudo: this.props.match.params.pseudo,
     recettes: {}
   }
   
+  componentDidMount() {
+    base.syncState(`/${this.state.pseudo}/recettes`, {
+      context: this,
+      state: 'recettes'
+    })
+  }
+
   chargerExemple = () => this.setState({ recettes })
 
   render () {
--- src/base.js
+++ src/base.js
@@ -3,9 +3,9 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'xxx',
-  authDomain: 'xxx',
-  databaseURL: 'xxx'
+  apiKey: "AIzaSyBRMhtr1fFRfMUuKY2KfYz9fP1DLNMRZGI",
+  authDomain: "boitte-recette.firebaseapp.com",
+  databaseURL: "https://boitte-recette-default-rtdb.europe-west1.firebasedatabase.app"
 })
 
 const base = Rebase.createClass(firebaseApp.database())
