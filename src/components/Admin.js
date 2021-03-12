722b02701a64e49132b9f51fa14f3ede135c884e
achrefabdennebi <achref.abdennebi@gmail.com>
2021-03-12 16:44:24 +0100

    feat: add provider facebook connect

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react'
 import AjouterRecette from './AjouterRecette'
 import AdminForm from './AdminForm'
+import Login from './Login'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import base, {firebaseApp} from '../base'
 class Admin extends Component {
+    state = {
+        uid: null,
+        chef: null
+    }
+
+    authenticate = () => {
+        const authProvider = new firebase.auth.FacebookAuthProvider();
+    
+        firebaseApp
+            .auth()
+            .signInWithPopup(authProvider)
+            .then(this.handleAuth)
+    }
+
+    handleAuth = authData => {
+        console.log(authData);
+    }
+
     render () {
         const { recettes, ajouterRecette, majRecette, chargerExemple, supprimerRecette } = this.props
+        
+        // Si l'utilisateur n'est pas connecté
+        if (!this.state.uid) {
+            return <Login authenticate={this.authenticate} />
+        }
+        
         return (
             <div className="cards">
                 <AjouterRecette ajouterRecette={ajouterRecette}></AjouterRecette>
